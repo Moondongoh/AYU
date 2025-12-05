19465bc8f4c6dd0251776e309ebda228be739dc9
Moondongoh <mdo0421@naver.com>
2025-12-05 10:58:37 +0900

    ONNX 버전 11로 변환

--- Capstone/ONNX/Convert.py
+++ Capstone/ONNX/Convert.py
@@ -2,4 +2,5 @@ from ultralytics import YOLO
 
 model = YOLO(r"C:\GIT\Capstone\Web\flask-template\weights\best.pt")
 model.export(format="onnx", opset=11)
+
 print("Model has been successfully converted to ONNX format..")
